8f8736a415a2d62236b4d0fa32ae2da0a8eb3c5d
Mihai Manuta <mihaimm@gmail.com>
2020-04-26 18:29:42 +0200

    premiers pas

---
+++ akm-runner/Assets/CharacBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacBehavior : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float vitesse;
+
+	// Start is called before the first frame update
+	void Start() {
+		rb.velocity += new Vector2(vitesse, 0);
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
